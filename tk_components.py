f0b8b9f569a57945913e1cbd64516ea3aa8b9493
LiuJLin <Liu_JLin@outlook.com>
2017-05-04 21:17:45 +0800

    add button, entry

--- tk_components.py
+++ tk_components.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter, sys
 from tkinter import *
 # window
 root = Tk()
@@ -11,5 +11,25 @@ for relief in [RAISED, SUNKEN, FLAT, RIDGE, GROOVE, SOLID]:
     # label , text
     Label(f, text = relief, width = 10).pack(side = LEFT)
     f.pack(side = LEFT, padx = 5, pady = 5)
-    
+# buttons
+Button(root, text = "Disabled", state = DISABLED).pack(side = LEFT)
+Button(root, text = "Cancel").pack(side = LEFT)
+Button(root, text = "OK").pack(side = LEFT)
+Button(root, text = "Quit", command = root.quit).pack(side = RIGHT)
+
+#entry 输入框
+f1 = Frame(root)
+Label(f1, text = "Standard Entry: ").pack(side = LEFT, padx = 5, pady = 10)
+e1 = StringVar()
+Entry(f1, width = 50, textvariable = e1).pack(side = LEFT)
+e1.set("输入框默认内容")
+f1.pack()
+
+f2 = Frame(root)
+Label(f2, text = "Disabled Entry: ").pack(side = LEFT, padx = 5, pady = 10)
+e2 = StringVar()
+Entry(f2, width = 50, textvariable = e2, state = DISABLED).pack(side = LEFT)
+e2.set("不可修改的内容")
+f2.pack()
+
 root.mainloop()
