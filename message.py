f4cbb873508f41a9a15953933bf7f2d3c6b520c5
LiuJLin <Liu_JLin@outlook.com>
2017-05-05 09:24:57 +0800

    message

---
+++ message.py
@@ -0,0 +1,5 @@
+from tkinter import *
+root = Tk()
+root.title("Top window")
+Message(root, text = "这是帮助文档！这是帮助文档", bg = 'blue', fg = 'ivory', relief = GROOVE ).pack(padx = 10, pady =10)
+root.mainloop()
